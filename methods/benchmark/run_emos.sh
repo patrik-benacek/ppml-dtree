f6f4b756faa00bbaca1df434c0d72ac0595053c1
Patrik Benacek <benacek.p@czechglobe.cz>
2021-01-20 10:05:07 +0100

    Cleaning EMOS model scripts.

--- methods/benchmark/run_emos.sh
+++ methods/benchmark/run_emos.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-fcst=24
-#vars="t2m prec24"
-vars="prec24"
-for var in $vars; do
-    #for year in 2015 2018; do
-    for year in 2018; do
-        src/emos_global_fixed_crch.R $var $year $fcst &> listings/emos_crch_global_${var}_f${fcst}_${year}.out 
-        src/emos_local_fixed_crch.R $var $year $fcst  &> listings/emos_crch_local_${var}_f${fcst}_${year}.out 
+# Run global- and local-EMOS methods for $TARGETS and $LEADTIMES. 
+# Training period is from $START_TRAIN_YEAR to 2018.
+# Testing period is 2019.
+
+LEADTIMES="24 240"
+TARGETS="t2m prec24"
+START_TRAIN_YEARS="2015 2018"
+
+for LEATIME in $LEADTIMES; do
+    for TARGET in $TARGETS; do
+        for START_TRAIN_YEAR in $START_TRAIN_YEARS; do
+            src/emos_global_fixed_crch.R $TARGET $LEADTIME $START_TRAIN_YEAR &> log_emos_glb_${var}_f${fcst}_${year}.out 
+            src/emos_local_fixed_crch.R $TARGET $LEADTIME $START_TRAIN_YEAR  &> log_emos_loc_${var}_f${fcst}_${year}.out 
+        done
     done
 done
